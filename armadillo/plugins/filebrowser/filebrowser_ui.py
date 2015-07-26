c4f016a174aab490a7965455bec4722d5d9b1071
cole <lucidlylogicole@gmail.com>
2015-07-26 00:17:17 -0500

    more style updates

--- armadillo/plugins/filebrowser/filebrowser_ui.py
+++ armadillo/plugins/filebrowser/filebrowser_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'filebrowser.ui'
 #
-# Created: Sat Jul 25 23:08:02 2015
+# Created: Sun Jul 26 00:05:23 2015
 #      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -38,7 +38,7 @@ class Ui_Form(object):
         self.le_root.setStyleSheet(_fromUtf8("background-color: qlineargradient(spread:pad, x1:1, y1:1, x2:1, y2:0, stop:0 rgba(32, 32, 32, 255), stop:0.0590909 rgba(41, 41, 41, 255), stop:0.922727 rgba(59, 59,59, 255), stop:1 rgba(71, 71, 71, 255));\n"
 "color:white;\n"
 "border:0px;\n"
-"border-bottom:1px solid gray;\n"
+"border-bottom:1px solid rgb(70,70,70);\n"
 "padding:2px;\n"
 "border-top-left-radius:3px;\n"
 "border-top-right-radius:3px;"))
